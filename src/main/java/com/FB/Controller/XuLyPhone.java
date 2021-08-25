3de2a6823b456b53fe01637e7f7df55584865e6c
Huan2302 <votuonghuan185@gmail.com>
2021-08-25 20:46:48 +0700

    handler

--- src/main/java/com/FB/Controller/XuLyPhone.java
+++ src/main/java/com/FB/Controller/XuLyPhone.java
@@ -1,7 +1,9 @@
 package com.FB.Controller;
 
 import com.FB.Dao.FindPhoneDao;
+import com.FB.Handle.Handle;
 import com.FB.Model.FBAccount;
+import com.FB.Model.ResultModel;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,72 +11,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @WebServlet(value = "/xu-ly")
 public class XuLyPhone extends HttpServlet {
+
     private FindPhoneDao findPhoneDao = new FindPhoneDao();
+
+    ResourceBundle resourceBundle = ResourceBundle.getBundle("globals");
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("html/text");
         resp.setCharacterEncoding("UTF-8");
 
+        Handle handle= new Handle();
         String uid = req.getParameter("uid");
         String[] uids = uid.split("\\r?\\n");
-        System.out.println();
-        ArrayList<String> lists = Pretreatment(uids);
+        ArrayList<String> lists = handle.Pretreatment(uids);
+        Collections.sort(lists);
 
-        System.out.println(uids.length);
-        System.out.println(lists.size());
+//        thread
+        String tables[] = resourceBundle.getString("table_test").split(",");
 
-        List<FBAccount> list = findPhoneDao.findAll();
-        StringBuffer listPhone = new StringBuffer();
-        String phone;
-        for (String item : lists){
-            phone = InterPolationSearch(list,item);
-            System.out.println(phone);
-            if (phone!=null){
-                listPhone.append(item+" || "+phone);
-            }
-        }
-        System.out.println(listPhone.toString());
-        req.setAttribute("listPhone",listPhone.toString());
+//        xử lý trả về phone
+        List<FBAccount> list_uid = findPhoneDao.findAll();
+        ResultModel rs = handle.InterPolationSearch(list_uid,0,lists,0);
+
+        req.setAttribute("listPhone",rs);
         req.getRequestDispatcher("/index.jsp").forward(req,resp);
     }
 
-//    public String InterPolationSearch(List<FBAccount> listUid,long left,long right, String uid ){
-//        int id = Integer.parseInt(uid);
-//        if (right>=1){
-//            long mid = left + (right - 1)/2;
-//            if (Integer.parseInt(listUid.get((int)mid).getFacebook_id()) == id){
-//                return listUid.get((int)mid).getPhone();
-//            }
-//            if (Integer.parseInt(listUid.get((int)mid).getFacebook_id()) > id){
-//                return InterPolationSearch(listUid,left,mid - 1,uid);
-//            }
-//            return InterPolationSearch(listUid,mid +1 ,right,uid);
-//        }
-//        return null;
-//    }
-
-    public String InterPolationSearch(List<FBAccount> listUid, String uid ){
-        for (FBAccount item:listUid){
-            if (uid.equals(item.getFacebook_id())){
-                return item.getPhone();
-            }
-        }
-        return null;
-    }
-
-    public ArrayList<String> Pretreatment(String[] lists){
-        ArrayList<String> arrStr = new ArrayList<>();
-        for (int i = 0; i< lists.length;i++){
-            if (!arrStr.contains(lists[i])){
-                arrStr.add(lists[i]);
-            }
-        }
-        return arrStr;
-    }
 }
+
+
+
